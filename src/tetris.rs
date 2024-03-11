0cc88ef06801bac6e415379c5a9c18ba2f26076d
Jacob Borch Madsen <jacob.borch@outlook.com>
2024-03-11 21:35:39 +0100

    more stuffs

--- src/tetris.rs
+++ src/tetris.rs
@@ -3,11 +3,23 @@ enum PlayerInput {
     Right,
     Rotate,
     Swap,
-    Place
+    Place,
 }
 
-
+#[derive(Clone)]
 struct Piece(Vec<(i32, i32)>);
+
+lazy_static::lazy_static! {
+    static ref O: Piece = Piece(vec![(0, 0), (1, 0), (0, 1), (1, 1)]);
+    static ref I: Piece = Piece(vec![(0, 0), (1, 0), (2, 0), (3, 0)]);
+    static ref S: Piece = Piece(vec![(0, 0), (-1, -1), (1, 0), (0, 1)]);
+    static ref Z: Piece = Piece(vec![(0, 0), (1, 1), (1, 0), (0, -1) ]);
+    static ref L: Piece = Piece(vec![(0, 0), (1, 0), (2, 0), (2, 1)]);
+    static ref J: Piece = Piece(vec![(0, 0), (1, 0), (2, 0), (2, -1)]);
+    static ref T: Piece = Piece(vec![(0, 0), (0, 1), (0, -1), (1, 0)]);
+
+    static ref PIECES: Vec<Piece> =  vec![O.clone(), I.clone(), S.clone(), Z.clone(), L.clone(), J.clone(), T.clone()];
+}
 
 impl Piece {
     fn rotated(&self) -> Self {
@@ -15,7 +27,8 @@ impl Piece {
     }
 
     fn random() -> Self {
-        todo!()
+        let i = fastrand::usize(0..7);
+        PIECES[i].clone()
     }
 }
 
@@ -34,7 +47,12 @@ struct Game {
 
 impl Game {
     pub fn new() -> Self {
-        todo!()
+        Self {
+            board: vec![vec![0; 10]; 20],
+            current_piece: Piece::random(),
+            piece_pos: (0, 10 / 2),
+            saved_piece: None,
+        }
     }
 
     fn spawn_new_piece(&mut self, opt_piece: Option<Piece>) {
@@ -49,24 +67,45 @@ impl Game {
     fn reset_piece(&mut self) {
         self.piece_pos = (0, self.board[0].len() / 2)
     }
+
+    fn can_move_right(&self) -> bool {
+        todo!()
+    }
+
+    fn can_move_left(&self) -> bool {
+        todo!()
+    }
 }
 
 impl TetrisGame for Game {
     fn player_input(&mut self, input: PlayerInput) {
         match input {
             PlayerInput::Left => {
-                
-            },
-            PlayerInput::Right => todo!(),
-            PlayerInput::Rotate => {
-                self.current_piece = self.current_piece.rotated()
-            },
-            PlayerInput::Swap => {
-                if let Some(piece) = &self.saved_piece {
-
+                if self.can_move_left() {
+                    self.piece_pos.1 -= 1
                 }
-            },
-            PlayerInput::Place => todo!(),
+            }
+            PlayerInput::Right => {
+                if self.can_move_right() {
+                    self.piece_pos.1 += 1
+                }
+            }
+            PlayerInput::Rotate => self.current_piece = self.current_piece.rotated(),
+            PlayerInput::Swap => {
+                let prev = &self.saved_piece;
+                if prev.is_some() {
+                    let tmp = self.current_piece.clone();
+                    self.current_piece = prev.clone().unwrap();
+                    self.saved_piece = Some(tmp);
+                    self.reset_piece()
+                } else {
+                    self.saved_piece = Some(self.current_piece.clone());
+                    self.spawn_new_piece(None);
+                }
+            }
+            PlayerInput::Place => {
+                todo!()
+            }
         }
     }
 
