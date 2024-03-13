21a2b78181ebe7f3d8a64ea43288f58d691d0ea0
Jacob Borch Madsen <jacob.borch@outlook.com>
2024-03-13 19:44:03 +0100

    more stuffs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,118 @@
-fn main() {
-    println!("Hello, world!");
+use bevy::ecs::query;
+use bevy::input::*;
+use bevy::math::vec3;
+use bevy::prelude::*;
+use bevy::render::mesh::shape::Circle;
+use bevy::render::mesh::shape::Cube;
+use bevy::render::mesh::shape::Quad;
+use bevy::render::mesh::PrimitiveTopology;
+use bevy::render::settings::RenderCreation;
+use bevy::render::settings::WgpuSettings;
+use bevy::render::RenderPlugin;
+use bevy::sprite::MaterialMesh2dBundle;
+use bevy::sprite::Mesh2d;
+use bevy::window::PresentMode;
+use tetris::tetris::Piece;
+use tetris::tetris::TetrisGame; // Import the PlayerInput trait
+use tetris::tetris::{Game, PlayerInput}; // Import the missing Input type
+
+struct TetrisPlugin;
+
+#[derive(Resource)]
+struct TetrisTimer(Timer);
+
+const BLOCK_SIZE: f32 = 20.0;
+
+
+impl Plugin for TetrisPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(Game::new()) // Your Tetris game instance
+            .insert_resource(TetrisTimer(Timer::from_seconds(1.5, TimerMode::Repeating)))
+            .add_systems(Startup, setup)
+            .add_systems(Update, player_input)
+            .add_systems(Update, game_tick)
+            .add_systems(Update, draw_current_piece)
+            .add_systems(Update, draw_board);
+    }
 }
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2dBundle::default());
+}
+
+
+fn game_tick(mut game: ResMut<Game>, time: Res<Time>, mut timer: ResMut<TetrisTimer>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        println!("tick!");
+        game.tick();
+    }
+
+}
+
+fn draw_board() {}
+
+fn piece_pos_to_coordinates((i, j): (usize, usize)) -> Vec3 {
+    vec3((10 * j) as f32, -((10 * i) as f32), 1.0)
+}
+
+
+#[derive(Component)]
+struct PieceComponent{}
+
+fn draw_current_piece(game: Res<Game>, old_piece: Query<Entity, With<PieceComponent>>, mut materials: ResMut<Assets<ColorMaterial>>, mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>) {
+    let (pos, piece) = game.get_current_piece();
+    let color = piece.color();
+    let vec3pos = piece_pos_to_coordinates(pos);
+    old_piece.for_each(|p| commands.get_entity(p).unwrap().despawn());
+    commands.spawn((MaterialMesh2dBundle {
+        mesh: bevy::sprite::Mesh2dHandle(meshes.add(Cube::new(BLOCK_SIZE).into())),
+        transform: Transform::from_translation(vec3pos),
+        material: materials.add(color.into()),
+        ..default()
+    }, PieceComponent{}));
+}
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins
+            .set(RenderPlugin {
+                render_creation: RenderCreation::Automatic(WgpuSettings {
+                    #[cfg(target_os = "windows")]
+                    backends: Some(bevy::render::settings::Backends::DX12),
+                    ..Default::default()
+                }),
+            })
+            .set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "Tetris".to_string(),
+                    present_mode: PresentMode::AutoNoVsync,
+                    ..default()
+                }),
+                ..default()
+            }),)
+        .add_plugins(TetrisPlugin)
+        .run();
+}
+
+fn player_input(keyboard_input: Res<Input<KeyCode>>, mut game: ResMut<Game>) {
+    if keyboard_input.pressed(KeyCode::Left) {
+        game.player_input(PlayerInput::Left);
+    }
+    if keyboard_input.pressed(KeyCode::Right) {
+        game.player_input(PlayerInput::Right);
+    }
+    if keyboard_input.pressed(KeyCode::Up) {
+        game.player_input(PlayerInput::Rotate);
+    }
+    if keyboard_input.pressed(KeyCode::Space) {
+        game.player_input(PlayerInput::Place);
+    }
+    if keyboard_input.pressed(KeyCode::Z) {
+        game.player_input(PlayerInput::Swap);
+    }
+}
+
--- src/tetris.rs
+++ src/tetris.rs
@@ -1,4 +1,6 @@
-enum PlayerInput {
+use bevy::{ecs::system::Resource, render::color::Color};
+
+pub enum PlayerInput {
     Left,
     Right,
     Rotate,
@@ -7,38 +9,58 @@ enum PlayerInput {
 }
 
 #[derive(Clone)]
-struct Piece(Vec<(i32, i32)>);
+//Offsets and color
+pub struct Piece(Vec<(i32, i32)>, usize);
 
 lazy_static::lazy_static! {
-    static ref O: Piece = Piece(vec![(0, 0), (1, 0), (0, 1), (1, 1)]);
-    static ref I: Piece = Piece(vec![(0, 0), (1, 0), (2, 0), (3, 0)]);
-    static ref S: Piece = Piece(vec![(0, 0), (-1, -1), (1, 0), (0, 1)]);
-    static ref Z: Piece = Piece(vec![(0, 0), (1, 1), (1, 0), (0, -1) ]);
-    static ref L: Piece = Piece(vec![(0, 0), (1, 0), (2, 0), (2, 1)]);
-    static ref J: Piece = Piece(vec![(0, 0), (1, 0), (2, 0), (2, -1)]);
-    static ref T: Piece = Piece(vec![(0, 0), (0, 1), (0, -1), (1, 0)]);
+    static ref O: Piece = Piece(vec![(0, 0), (1, 0), (0, 1), (1, 1)], 1);
+    static ref I: Piece = Piece(vec![(0, 0), (1, 0), (2, 0), (3, 0)], 2);
+    static ref S: Piece = Piece(vec![(0, 0), (-1, -1), (1, 0), (0, 1)], 3);
+    static ref Z: Piece = Piece(vec![(0, 0), (1, 1), (1, 0), (0, -1) ], 4);
+    static ref L: Piece = Piece(vec![(0, 0), (1, 0), (2, 0), (2, 1)], 5);
+    static ref J: Piece = Piece(vec![(0, 0), (1, 0), (2, 0), (2, -1)], 6);
+    static ref T: Piece = Piece(vec![(0, 0), (0, 1), (0, -1), (1, 0)], 7);
 
     static ref PIECES: Vec<Piece> =  vec![O.clone(), I.clone(), S.clone(), Z.clone(), L.clone(), J.clone(), T.clone()];
 }
 
+
 impl Piece {
     fn rotated(&self) -> Self {
-        Self(self.0.iter().map(|&(i, j)| (j, -i)).collect())
+        Self(self.0.iter().map(|&(i, j)| (j, -i)).collect(), self.1)
     }
 
     fn random() -> Self {
-        let i = fastrand::usize(0..7);
+        let i = fastrand::usize(0..PIECES.len());
         PIECES[i].clone()
+    }
+
+    pub fn color(&self) -> Color {
+        match self.1 {
+            1 => Color::YELLOW,
+            2 => Color::TURQUOISE,
+            3 => Color::RED,
+            4 => Color::GREEN,
+            5 => Color::ORANGE,
+            6 => Color::BLUE,
+            7 => Color::PURPLE,
+            _ => unreachable!()
+        }
     }
 }
 
-trait TetrisGame {
+pub trait TetrisGame {
     fn player_input(&mut self, input: PlayerInput);
 
     fn tick(&mut self);
+
+    fn get_board(&self) -> &Vec<Vec<usize>>;
+
+    fn get_current_piece(&self) -> ((usize, usize), Piece);
 }
 
-struct Game {
+#[derive(Resource)]
+pub struct Game {
     board: Vec<Vec<usize>>,
     current_piece: Piece,
     piece_pos: (usize, usize),
@@ -68,12 +90,51 @@ impl Game {
         self.piece_pos = (0, self.board[0].len() / 2)
     }
 
-    fn can_move_right(&self) -> bool {
-        todo!()
+    fn can_move(&self, dx: i32, dy: i32) -> bool {
+        self.current_piece.0.iter().all(|&(x, y)| {
+            let new_x = x + dx + self.piece_pos.0 as i32;
+            let new_y = y + dy + self.piece_pos.1 as i32;
+
+            new_x >= 0 && new_x < self.board.len() as i32 && // Check vertical boundaries
+            new_y >= 0 && new_y < self.board[0].len() as i32 && // Check horizontal boundaries
+            self.board[new_x as usize][new_y as usize] == 0 // Check for no blockage
+        })
     }
 
     fn can_move_left(&self) -> bool {
-        todo!()
+        self.can_move(0, -1)
+    }
+    
+    fn can_move_right(&self) -> bool {
+        self.can_move(0, 1)
+    }
+    
+    fn can_move_down(&self) -> bool {
+        self.can_move(1, 0)
+    }
+    
+    fn lock_piece(&mut self) {
+        // Place the piece on the board
+        for &(x, y) in &self.current_piece.0 {
+            let board_x = (self.piece_pos.0 as i32 + x) as usize;
+            let board_y = (self.piece_pos.1 as i32 + y) as usize;
+            if board_x < self.board.len() && board_y < self.board[0].len() {
+                self.board[board_x][board_y] = 1;
+            }
+        }
+
+        // Check for and clear any full lines
+        self.remove_full_lines();
+
+        // Spawn a new piece
+        self.spawn_new_piece(None);
+    }
+
+    fn remove_full_lines(&mut self) {
+        self.board.retain(|row| row.iter().any(|&cell| cell == 0));
+        while self.board.len() < 20 { // Assuming 20 rows in the standard Tetris game
+            self.board.insert(0, vec![0; 10]); // Re-add empty rows at the top
+        }
     }
 }
 
@@ -104,12 +165,24 @@ impl TetrisGame for Game {
                 }
             }
             PlayerInput::Place => {
-                todo!()
+                self.lock_piece()
             }
         }
     }
 
     fn tick(&mut self) {
-        todo!()
+        if !self.can_move_down() {
+            self.lock_piece()
+        } else {
+            self.piece_pos.0 += 1
+        }
+    }
+
+    fn get_board(&self) -> &Vec<Vec<usize>> {
+        &self.board
+    }
+
+    fn get_current_piece(&self) -> ((usize, usize), Piece) {
+        (self.piece_pos, self.current_piece.clone())
     }
 }
